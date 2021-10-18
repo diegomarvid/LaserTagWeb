742539cdfb826bfa4b29de985729bf7981e4f2f0
Diego Marvid <33181424+diegomarvid@users.noreply.github.com>
2021-10-18 16:22:10 -0300

    comment first static

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ let server = require('http').Server(app);
 app.use(express.json())
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, 'client', 'public')))
+// app.use(express.static(path.join(__dirname, 'client', 'public')))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true })); 
